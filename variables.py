4cf8c8586b1c8f6729a611b887f16ba10a108180
Adrian Contreras <acontrerasregueiro@gmail.com>
2023-12-05 09:37:43 +0100

    ejemplos de asignación de variables

---
+++ variables.py
@@ -0,0 +1,25 @@
+#EJEMPLOS ASIGNACION DE VARIABLES
+
+x = 1
+print(type(x))
+print(x)
+
+x,y = 1,2
+print(' X = ',x ,' Y = ', y )
+
+#EJEMPLO ASIGNANDO A TODAS EL MISMO VALOR
+x = y = z = 20
+print(x,y,z)
+
+#CURIOSIDAD
+#EJEMPLO DE ASIGNACION MULTIPLE
+x = 4
+y = 5
+x,y = y,x
+print(x,y)
+
+
+
+#ERROR DE DEBEN ASIGNAR TANTOS VALORES COMO VARIABLES
+'''x,y,z = 1,2,3,4
+print(x,y,z)'''
