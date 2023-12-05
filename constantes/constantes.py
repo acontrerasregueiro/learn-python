0c7071fbe278f2beb4b76953f4044bfcfa79d416
Adrian Contreras <acontrerasregueiro@gmail.com>
2023-12-05 09:02:53 +0100

    CONSTANTES DESDE FICHERO EXTERNO

---
+++ constantes/constantes.py
@@ -0,0 +1,3 @@
+USUARIO = "test_usuario"
+CONTRASEÑA = "test_contraseña"
+SERVIDOR = 'http:\\\\google.es'  #Ponemos 4 "\" para mostrar \\
---
+++ constantes/ejemplo1.py
@@ -0,0 +1,3 @@
+import constantes as constantes
+print("Ejemplo lectura constantes desde modulo externo")
+print("Accediendo a la pagina {0} con usuario {1} y contraseña {2}".format(constantes.SERVIDOR, constantes.USUARIO, constantes.CONTRASEÑA))
