a26fd4f96ed4011bfbf497de956ca53b59abe67b
Adrian Contreras <acontrerasregueiro@gmail.com>
2023-12-05 09:05:49 +0100

    Añadiendo ejemplos de lectura de Constantes desde fichero externO

--- constantes/ejemplo1.py
+++ constantes/ejemplo1.py
@@ -1,3 +1,6 @@
 import constantes as constantes
 print("Ejemplo lectura constantes desde modulo externo")
 print("Accediendo a la pagina {0} con usuario {1} y contraseña {2}".format(constantes.SERVIDOR, constantes.USUARIO, constantes.CONTRASEÑA))
+
+print(constantes.SERVIDOR)
+print("El usuario es ", constantes.USUARIO  , " y la contraseña es ", constantes.CONTRASEÑA)
