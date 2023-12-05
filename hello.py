dfeff7f632f52491215620179435e977d4f8067f
adrian contreras <acontrerasregueiro@gmail.com>
2023-12-05 07:38:09 +0100

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+print("Hello world")
+print('Hello')
+
+a = 1
+b = "python"
+#print (a + b)    ERROR DIFERENTES TIPOS
+b = str(a) + b
+print(b)
+
+#ASIGNACION MUTIPLE DE VARIABLES
+x, y, z= 2 , 17.23, "Python"
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+#ASIGNACION MISMO VALOR A VARIABLES
+
+x = y = z = 23
+print(x)
+print(y)
+print(z)
