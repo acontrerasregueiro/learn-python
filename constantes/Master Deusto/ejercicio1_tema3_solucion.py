d600d7efddaa2606d4d865054ea4413e2b935d3c
Adrian Contreras <acontrerasregueiro@gmail.com>
2023-12-12 10:23:39 +0100

    Ejercicios master deusto y pequeñas modificaciones diccionarios

---
+++ constantes/Master Deusto/ejercicio1_tema3_solucion.py
@@ -0,0 +1,70 @@
+
+#Inicializamos variable, el juego comienza con la ronda 1
+Nombre_Jugador_1 = input('Introduce el nombre del primer jugador: ')
+Nombre_Jugador_2 = input('Introduce el nombre del segundo jugador: ')
+Nombre_Jugador_3 = input('Introduce el nombre del tercer jugador: ')
+Nombre_Jugador_4 = input('Introduce el nombre del cuarto jugador: ')
+
+#Utilizaremos un diccionario para almacenar nombre y puntuación de cada jugador, partimos de 121 y vamos restando
+puntuacion = {}
+#Inicializamos pares clave/valor
+puntuacion[Nombre_Jugador_1] = 121
+puntuacion[Nombre_Jugador_2] = 121
+puntuacion[Nombre_Jugador_3] = 121
+puntuacion[Nombre_Jugador_4] = 121
+
+#Primera ronda
+terminado = False
+ronda = 1
+Puntos_Jugador_1 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_1, ronda)))
+Puntos_Jugador_2 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_2, ronda)))
+Puntos_Jugador_3 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_3, ronda)))
+Puntos_Jugador_4 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_4, ronda)))
+
+puntuacion[Nombre_Jugador_1] = 121 - Puntos_Jugador_1
+puntuacion[Nombre_Jugador_2] = 121 - Puntos_Jugador_2
+puntuacion[Nombre_Jugador_3] = 121 - Puntos_Jugador_3
+puntuacion[Nombre_Jugador_4] = 121 - Puntos_Jugador_4
+
+#Chequeamos si ha finalizado el juego
+if puntuacion[Nombre_Jugador_1] <= 0 or puntuacion[Nombre_Jugador_2] <= 0 or puntuacion[Nombre_Jugador_3] <= 0 or puntuacion[Nombre_Jugador_4] <= 0:
+    terminado = True
+    print('El juego ha finalizado en la ronda nº ' + str(ronda))
+
+if  (terminado == False):
+    print('Continuamos a la ronda 2')  
+    ronda = 2
+    Puntos_Jugador_1 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_1, ronda)))
+    Puntos_Jugador_2 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_2, ronda)))
+    Puntos_Jugador_3 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_3, ronda)))
+    Puntos_Jugador_4 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_4, ronda)))
+
+    #Actualizamos puntuaciones
+    puntuacion[Nombre_Jugador_1] = puntuacion[Nombre_Jugador_1] - Puntos_Jugador_1
+    puntuacion[Nombre_Jugador_2] = puntuacion[Nombre_Jugador_2] - Puntos_Jugador_2
+    puntuacion[Nombre_Jugador_3] = puntuacion[Nombre_Jugador_3] - Puntos_Jugador_3
+    puntuacion[Nombre_Jugador_4] = puntuacion[Nombre_Jugador_4] - Puntos_Jugador_4
+    #Comprobamos puntuaciones
+    if puntuacion[Nombre_Jugador_1] <= 0 or puntuacion[Nombre_Jugador_2] <= 0 or puntuacion[Nombre_Jugador_3] <= 0 or puntuacion[Nombre_Jugador_4] <= 0:
+        print('El juego ha finalizado en la ronda nº ' + str(ronda))
+    else:
+        print('Continuamos a la ronda 3')
+
+if  terminado == False:  
+    ronda = 3
+    Puntos_Jugador_1 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_1, ronda)))
+    Puntos_Jugador_2 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_2, ronda)))
+    Puntos_Jugador_3 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_3, ronda)))
+    Puntos_Jugador_4 = int(input('Introduce la puntuación total de {0} en la ronda {1} :'.format(Nombre_Jugador_4, ronda)))
+
+    #Actualizamos puntuaciones
+    puntuacion[Nombre_Jugador_1] = puntuacion[Nombre_Jugador_1] - Puntos_Jugador_1
+    puntuacion[Nombre_Jugador_2] = puntuacion[Nombre_Jugador_2] - Puntos_Jugador_2
+    puntuacion[Nombre_Jugador_3] = puntuacion[Nombre_Jugador_3] - Puntos_Jugador_3
+    puntuacion[Nombre_Jugador_4] = puntuacion[Nombre_Jugador_4] - Puntos_Jugador_4
+    #Comprobamos puntuaciones
+    if puntuacion[Nombre_Jugador_1] <= 0 or puntuacion[Nombre_Jugador_2] <= 0 or puntuacion[Nombre_Jugador_3] <= 0 or puntuacion[Nombre_Jugador_4] <= 0:
+        print('El juego ha finalizado en la ronda nº ' + str(ronda))
+    #Si nadia ha ganado se termina el juego (ronda 3)
+    else:
+        print("JUEGO FINALIZADO SIN GANADOR")
--- diccionarios.py
+++ diccionarios.py
@@ -1,4 +1,7 @@
 #Ejercicios y métodos de diccionarios
+#Un Dicionario en la parte 'valor' puede contener cualquier otro tipo de datos,
+#Es decir puede tener otro diccionario, una lista, entero, cadena,etc...
+
 
 jugadores = {
     #'clave' : 'valor',
@@ -9,6 +12,8 @@ jugadores = {
     '8': 'Iniesta',
     '10': 'Messi'
 }
+
+
 
 print(jugadores['1'])
 print(jugadores)
@@ -34,3 +39,82 @@ for clave in jugadores:
 #Obtener todo
 for clave in jugadores:
     print('el Número del jugador es {0} y el nombre es {1}'.format(clave,jugadores[clave]))
+
+ 
+#Métodos
+
+jugadores.clear()  #Elimina EL CONTENIDO del diccionario
+print(jugadores)
+
+'''
+del jugadores      #Con del eliminamos el diccionario       
+print(jugadores)
+'''
+
+jugadores = {
+    #'clave' : 'valor',
+    '1': 'Valdes',
+    '2': 'Puyol',
+    '3': 'Xavi',
+    '4': 'Guardiola',
+    '8': 'Iniesta',
+    '10': 'Messi' 
+}
+print(jugadores)
+#Método get devuelve el valor cuya clave coincide con el parámetro pasado
+print(jugadores.get('1'))
+#SI SE ESPECIFICA EL SEGUNDO PARAMETRO Y NO ENCUENTRA UN VALOR ASOCIADO A ESA CLAVE NOS LO DEVUELVE
+print(jugadores.get('50','NOT FOUND'))  
+
+#Método items()
+print(jugadores.items())
+#Si lo pasamos a una lista con list() podemos iterarlos
+lista= list(jugadores.items())
+print(lista[2])
+#Y acceder a sus elementos por separados
+print(lista[2][0])
+print(lista[2][1]) 
+print(type(lista))
+
+#Metodo pop() y popitem()
+#Elimina un elemento aleatorio, si le pasamos argumento elimina el elemento de esa posicion
+jugadores.popitem()
+print(jugadores)
+print(jugadores.values())
+#print(jugadores.keys())
+
+claves= list(jugadores.keys())
+print(claves[0])
+# Método update si existe la clave actualiza ese valor,
+# Si no existe la crea 
+
+jugadores.update({  
+    #'clave' : 'valor',
+    '1': 'Valdes',
+    '29': 'Puyol',
+    '39': 'Xavi',
+    '49': 'Guardiola',
+    '89': 'Iniesta',
+    '109': 'Messi',
+    '9': 'Busquets' 
+})
+print(jugadores)
+
+#Método pop() elimina el par clave:valor especificado en el argumento
+
+jugadores.pop('1')
+print(jugadores)
+
+dic = dict(
+Nombre="Josep",
+Tel=688777555,
+Material=["PC", "Teclado", "WebCam"]
+)
+print("fromkeys")
+#fromkeys() - Esta función nos ayuda a generar un diccionario a partir de las clave
+variables = ('Nombre', 'Tel', 'Material')
+dic = dict.fromkeys(variables)
+print(dic)
+dic = dict.fromkeys(variables, "Rellenar")
+print(dic)
+print("\n")
