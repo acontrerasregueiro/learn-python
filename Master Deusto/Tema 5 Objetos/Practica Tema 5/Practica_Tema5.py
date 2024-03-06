d882da6ef07f5c0dcff73b5c9290f612dd0b13fc
unknown <acontrerasregueiro@gmail.com>
2024-03-06 10:19:45 +0100

    Relacionando Clases

--- Master Deusto/Tema 5 Objetos/Practica Tema 5/Practica_Tema5.py
+++ Master Deusto/Tema 5 Objetos/Practica Tema 5/Practica_Tema5.py
@@ -1,1 +1,41 @@
 import clase_persona as clase
+
+cancion = clase.Cancion("Hola", "ACDC" , "Rock")
+persona = clase.Persona("34898103L","Javier","Lopez" , "Lopez", "Madrid" ,
+                       "España", cancion)
+
+
+#favorita = clase.Favoritas("34898103L","Javier","Lopez" , "Lopez", "Madrid" ,
+#                       "España","Hola", "ACDC" , "Rock")
+#print(cancion.titulo)
+""""""
+def anadir_persona():
+    pass
+
+def anadir_cancion():
+    pass
+
+def ver_favoritas():
+    pass
+
+def main():
+    """Funcion principal"""
+    while True:
+        print("""\nSelecciona una opción:
+            1) Añadir contacto
+            2) Borrar contacto
+            3) Ver la agenda
+            4) Salir
+            """)
+        opcion = input('Elija una opción del menú: ')
+        match opcion:
+            case "1": anadir_persona()
+            case "2": anadir_cancion()
+            case "3": ver_favoritas()
+            case "4": quit()
+            case _:
+                print("Opción no válida")
+
+
+if __name__ == "__main__":
+    main()
--- Master Deusto/Tema 5 Objetos/Practica Tema 5/clase_persona.py
+++ Master Deusto/Tema 5 Objetos/Practica Tema 5/clase_persona.py
@@ -1,27 +1,45 @@
 #En este modulo definimos las clases necesarias 
+class Cancion():
+    def __init__(self, titulo, grupo, genero):
+        self.titulo = titulo
+        self.grupo = grupo
+        self.genero = genero
 
 class Persona():
     
-    def __init__(self,dni,nombre,apellidos,apellido_2,poblacion,pais):
-        dni = self.dni
-        nombre = self.nombre
-        apellido = self.apellido
-        apellido_2 = self.apellido_2
-        poblacion = self.poblacion
-        pais = self.pais
+    def __init__(self,dni,nombre,apellido,apellido_2,poblacion,pais,cancion):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+        self.apellido_2 = apellido_2
+        self.poblacion = poblacion
+        self.pais = pais
+        self.cancion = cancion
         
 
-class Cancion():
-    def __init__(self, titulo, grupo, genero):
-        titulo = self.titulo
-        grupo = self.grupo
-        genero = self.genero
 
 class Favoritas(Persona,Cancion):
     
-    def __init__(self):
-        Persona.__init__(self, dni, nombre, apellido, apellido_2, poblacion, pais)
-        Cancion.__init__(self, titulo, grupo, genero)
-    
+    def __init__(self,persona,cancion):
+        """Cancion.__init__(self, cancion.titulo, cancion.grupo, cancion.genero)
+        Persona.__init__(self, persona.dni, persona.nombre, persona.apellido, persona.apellido_2
+                         , persona.poblacion, persona.pais,cancion)"""
+        #Cancion.__init__(self, titulo, grupo, genero)
+        #Persona.__init__(self, dni, nombre, apellido, apellido_2, poblacion, pais,cancion)
+        self.dni = persona.dni
+        self.titulo = cancion.titulo
+        
     def mostrar_favorita(self):
         print("La cancion favorita de {} es {}." .format(self.dni,self.titulo))            
+
+
+cancion = Cancion("Hola", "ACDC" , "Rock")
+persona = Persona("34898103L","Javier","Lopez" , "Lopez", "Madrid" ,
+                       "España", cancion)
+
+
+favorita = Favoritas(persona,cancion)
+print(favorita.dni)
+print(favorita.titulo)
+favorita.mostrar_favorita()
+#favorita.mostrar_favorita()
