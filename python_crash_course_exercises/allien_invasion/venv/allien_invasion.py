4332fc30f5f010755a853e9d62b312f6c61e24f1
unknown <acontrerasregueiro@gmail.com>
2024-04-30 11:12:18 +0200

    lo dejamos en la pagina 232 creating a settings Class

--- python_crash_course_exercises/allien_invasion/venv/allien_invasion.py
+++ python_crash_course_exercises/allien_invasion/venv/allien_invasion.py
@@ -7,9 +7,11 @@ class AllienInvasion:
     def __init__(self):
         """Iniciamos el juego y creamos recursos"""
         pygame.init()
+        self.clock = pygame.time.Clock()
         
-        self.screen = pygame.display.set_mode((1200,800))
-        pygame.display.set_caption("Allien Invasion")
+        self.screen = pygame.display.set_mode((1200,800))  #Resolucion
+        pygame.display.set_caption("Allien Invasion")   #Titulo
+        self.bg_color = (230,230,230)   #Background color
         
     def run_game(self):
         """Bucle inicial para lanzar el juego"""
@@ -19,8 +21,10 @@ class AllienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            self.screen.fill(self.bg_color) #Redibujamos la pantalla en cada vuelta del loop
             #Mostramos la pantalla
             pygame.display.flip()
+            self.clock.tick(60)  #Intentamos que corra a 60FPS
 
 if __name__ == '__main__':
     #Creamos una instancia del juego y la ejecutamos
